2dcff64957d82b80bf37afb6315900214c7d34a6
vladimir <vladimirgiraldo@gmail.com>
2023-11-18 20:56:45 +0000

    change salute to hello Continuos Deliery

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello Continuos Delivery!";
 	}
 
 	public static void main(String[] args) {
